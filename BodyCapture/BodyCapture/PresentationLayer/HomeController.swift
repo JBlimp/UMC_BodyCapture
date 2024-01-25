eef14aedb4c1664673b53f4573f8e407429ef983
0ne <tkdwns285@hanyang.ac.kr>
2024-01-25 23:22:52 +0900

    SceneDelegate에 isTokenValid 함수 개요 (참 : MainTab, 거짓: Login)

---
+++ BodyCapture/BodyCapture/PresentationLayer/HomeController.swift
@@ -0,0 +1,8 @@
+//
+//  HomeController.swift
+//  BodyCapture
+//
+//  Created by 하상준 on 1/25/24.
+//
+
+import Foundation
---
+++ BodyCapture/BodyCapture/PresentationLayer/StoreController.swift
@@ -0,0 +1,8 @@
+//
+//  StoreController.swift
+//  BodyCapture
+//
+//  Created by 하상준 on 1/25/24.
+//
+
+import Foundation
