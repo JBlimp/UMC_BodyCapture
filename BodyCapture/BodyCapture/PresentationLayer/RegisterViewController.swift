dd176b925225bb62c4e93d3cc9bba941299f7644
JBlimp <65824519+JBlimp@users.noreply.github.com>
2024-01-31 19:38:17 +0900

    RegisterViewController

---
+++ BodyCapture/BodyCapture/PresentationLayer/RegisterViewController.swift
@@ -0,0 +1,12 @@
+//
+//  RegisterViewController.swift
+//  BodyCapture
+//
+//  Created by junseok on 1/31/24.
+//
+
+import UIKit
+
+class RegisterViewController : UIViewController {
+    
+}
--- BodyCapture/BodyCapture/SceneDelegate.swift
+++ BodyCapture/BodyCapture/SceneDelegate.swift
@@ -117,7 +117,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 }
             }
         }
-
     }
     
     func sceneDidDisconnect(_ scene: UIScene) {
